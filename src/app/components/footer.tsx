280072b257fda65293763b437053833efa9981c4
Miftakhul Rizky <m.miftakhul.rizky4@gmail.com>
2025-09-07 13:16:15 +0700

    style(footer): improve responsive layout for mobile devices
    Make footer links stack vertically on mobile and horizontally on larger screens to enhance mobile user experience

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -3,7 +3,7 @@
 export default function Footer() {
   return (
     <div className="w-full max-w-5xl mx-auto px-6 py-6 bg-white/5 border-1 border-white/10">
-      <div className="flex justify-between px-6">
+      <div className="flex flex-col self-center md:flex-row md:justify-between px-6">
         <ul className="flex gap-4 text-sm">
           <li><a href="/home">Home</a></li>
           <li><a href="/project">Project</a></li>
